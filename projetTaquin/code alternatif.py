32d7e11fcc7a0f6a2a554e6f2d2ec80de04de004
Laly Azzopardi <laly.azzopardi@ens.uvsq.fr>
2023-04-08 16:42:50 +0200

    ajout 2

--- projetTaquin/code alternatif.py
+++ projetTaquin/code alternatif.py
@@ -3,8 +3,9 @@ from tkinter import *
 
 DELTA = 20
 DIST = 10
-moving = 0
+moving_tiles = 0
 
+#Fonction verification des mouvements 
 def get_moves(origin, empty):
     i, j = origin
     ii, jj = empty
@@ -18,3 +19,17 @@ def get_moves(origin, empty):
 
     return [((ii - dirx*k, jj - diry*k), (ii - dirx*(k-1), jj - diry*(k-1)))
             for k in range(norm, 0, -1)]
+
+
+def animate_tile(item, target_pos, direction):
+    global moving_tiles
+    start_pos = cnv.coords(item)[:2]  # get the x and y coordinates of the top left corner of the item
+    x_diff, y_diff = [t - s for s, t in zip(start_pos, target_pos)]
+    u, v = direction
+    distance = u * x_diff + v * y_diff
+    if distance > DIST:
+        cnv.move(item, u * DIST, v * DIST)
+        cnv.after(DELTA, animate_tile, item, target_pos, direction)
+    else:
+        cnv.move(item, x_diff, y_diff)
+        moving_tiles -= 1
